0e8278b1cebc6068998593bdd33eb9bd272a1060
山下竜二 <>
2019-10-07 01:04:37 +0900

    no message

--- app/src/main/java/com/example/ryuji_mvvm_architecture/main/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/ryuji_mvvm_architecture/main/fragment/FirstFragment.kt
@@ -1,19 +1,14 @@
 package com.example.ryuji_mvvm_architecture.main.fragment
 
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.ryuji_mvvm_architecture.R
+import com.example.moeidbannerlibrary.banner.BaseBannerAdapter
 import com.example.ryuji_mvvm_architecture.base.BaseFragment
 import com.example.ryuji_mvvm_architecture.base.ReceiverType
 import com.example.ryuji_mvvm_architecture.databinding.FragmentFirstBinding
 import com.example.ryuji_mvvm_architecture.main.FirstScreenState.INITIAL
 import com.example.ryuji_mvvm_architecture.main.FirstScreenState.NEXT
 import com.example.ryuji_mvvm_architecture.main.MainViewModel
-import com.example.ryuji_mvvm_architecture.main.MyAdapter
-import com.example.ryuji_mvvm_architecture.main.Version
 import com.example.ryuji_mvvm_architecture.main.fragment.FirstFragment.FirstReceiverType.CLICK_NEXT_BUTTON
-import com.example.ryuji_mvvm_architecture.util.Utility
+
 
 class FirstFragment : BaseFragment<MainViewModel, FragmentFirstBinding>(MainViewModel::class.java) {
 
@@ -25,7 +20,7 @@ class FirstFragment : BaseFragment<MainViewModel, FragmentFirstBinding>(MainView
         CLICK_NEXT_BUTTON to { _ -> viewModel.dispatch(NEXT) }
     )
 
-    override fun layoutResource() = R.layout.fragment_first
+    override fun layoutResource() = com.example.ryuji_mvvm_architecture.R.layout.fragment_first
 
     override fun bindViewModel(viewModel: MainViewModel) {
         binding.viewModel = viewModel
@@ -36,19 +31,16 @@ class FirstFragment : BaseFragment<MainViewModel, FragmentFirstBinding>(MainView
         viewModel.dispatch(INITIAL)
 
         // TODO: カルーセル出してみる!!
-        // https://github.com/AndroidCodility/HorizontalRecyclerview
-        // https://stackoverflow.com/questions/52590517/horizontal-recyclerview-has-item-cardview-bottom-textview-part-not-fully-display
-        context?.let {
-            if (Utility.isOnline(it)) {
-                val version = ArrayList<Version>()
-                version.addAll(Version.getList())
-                binding.recyclerView.apply {
-                    layoutManager = LinearLayoutManager(it, LinearLayout.HORIZONTAL, false)
-                    adapter = MyAdapter(version)
-                }
-            } else {
-                Toast.makeText(it, "No internet available..!!", Toast.LENGTH_SHORT).show()
-            }
+        val urls = mutableListOf<String>(
+            "https://raw.githubusercontent.com/AndroidCodility/StaggeredRecyclerView/master/images/one.jpg",
+            "https://raw.githubusercontent.com/AndroidCodility/StaggeredRecyclerView/master/images/nine.jpg",
+            "https://raw.githubusercontent.com/AndroidCodility/StaggeredRecyclerView/master/images/ten.jpg"
+        )
+        val bannerAdapter = BaseBannerAdapter(activity, urls)
+        bannerAdapter.setOnBannerItemClickListener { }
+        binding.banner.apply {
+            setAdapter(bannerAdapter)
+            setAutoPlayDuration(3000)
         }
     }
 }
